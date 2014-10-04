013062faf0e89830c8fd5406eb99e26fef405980
Dougal Scott <Dougal.Scott@gmail.com>
2014-10-04 18:53:03 +1000

    Change richness description

---
+++ moo/planet/migrations/0007_auto_20141004_0744.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('planet', '0006_planet_climate'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='planet',
+            name='richness',
+            field=models.CharField(default=b'A', max_length=2, choices=[(b'UP', b'Ultra Poor'), (b'P', b'Poor'), (b'A', b'Abundant'), (b'R', b'Rich'), (b'UR', b'Ultra Rich')]),
+        ),
+    ]
