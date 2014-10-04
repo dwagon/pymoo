585883d3e547a14883b265a12a54716cc8a04bca
Dougal Scott <Dougal.Scott@gmail.com>
2014-10-04 17:30:17 +1000

    Fix game serialiser

--- moo/game/serializers.py
+++ moo/game/serializers.py
@@ -4,9 +4,13 @@ from rest_framework import serializers
 
 class GameSerializer(serializers.ModelSerializer):
     systems = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    max_x = serializers.Field(source='size.max_x')
+    max_y = serializers.Field(source='size.max_y')
+    numsystems = serializers.Field(source='size.numsystems')
 
     class Meta:
         model = Game
-        fields = ('turn', 'max_x', 'max_y', 'numplanets', 'systems')
+        read_only_fields = ('turn', 'name', 'numplayers')
+        exclude = ('size',)
 
 # EOF
