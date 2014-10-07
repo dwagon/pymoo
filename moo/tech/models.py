360be5fa12d3f44bd6e7254cea8344a9edb828bd
Dougal Scott <Dougal.Scott@gmail.com>
2014-10-07 19:16:43 +1100

    Tech Categories

--- moo/tech/models.py
+++ moo/tech/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
 
-class Tech(models.Model):
+class TechCategory(models.Model):
+    CATEG_CHOICES = (
+       ('CN', 'Construction'), ('PO', 'Power'),
+       ('CH', 'Chemistry'), ('SO', 'Sociology'),
+       ('CM', 'Computers'), ('BL', 'Biology'),
+       ('PH', 'Physics'), ('FF', 'Force Fields'),
+       ('UN', 'Unknown')
+    )
     name = models.CharField(max_length=250)
     cost = models.IntegerField()
+    category = models.CharField(max_length=2, choices=CATEG_CHOICES)
+
+
+class Tech(models.Model):
+    name = models.CharField(max_length=250)
+    categ = models.ForeignKey(TechCategory)
+    desc = models.CharField(max_length=1024, default='')
+    researchable = models.BooleanField(default=True)
 
 
 # EOF
