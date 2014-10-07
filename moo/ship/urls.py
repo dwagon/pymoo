e73e91263150f2ad27d8beafc710b1429c65ef3a
Dougal Scott <Dougal.Scott@gmail.com>
2014-10-07 14:30:13 +1100

    Allow a ships destination to be set

--- moo/ship/urls.py
+++ moo/ship/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 
-from .views import details
+from .views import details, setDestination
 
 urlpatterns = patterns(
     '',
     url(r'^details/(?P<ship_id>\d+)$', details, name='shipDetails'),
+    url(r'^setDestination/(?P<ship_id>\d+)/(?P<dest_id>\d+)$', setDestination, name='setDestination'),
 )
 
 # EOF
--- moo/ship/views.py
+++ moo/ship/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Ship
+from system.models import System
 
 
 def details(request, ship_id):
     d = {}
     d['ship'] = get_object_or_404(Ship, pk=ship_id)
     return render(request, 'ship/details.html', d)
+
+
+def setDestination(request, ship_id, dest_id):
+    sh = get_object_or_404(Ship, pk=ship_id)
+    sy = get_object_or_404(System, pk=dest_id)
+    sh.destsystem = sy
+    sh.save()
+    return render(request, 'ship/details.html', {'ship': sh})
 
 # EOF
